f285d8dd3ee7027258471eddb04b3d2b3acd59f2
Luisito Canlas <Luisito.Canlas@tlgcohort.com>
2023-05-18 10:22:40 -0700

    Game Information presented to player, AB#81

--- src/main/java/com/fishekai/engine/Fishekai.java
+++ src/main/java/com/fishekai/engine/Fishekai.java
@@ -1,7 +1,18 @@
 package com.fishekai.engine;
 
-public class Fishekai {
+import com.fishekai.utilities.SplashApp;
+
+public class Fishekai implements SplashApp {
+    //fields
+    private final Introduction intro = new Introduction();
 
 
+    // methods
+    public void start() {
+        // show the intro
+        intro.showIntro();
+        // prompt to press any key to continue
+
+    }
 
 }
--- src/main/java/com/fishekai/game/Main.java
+++ src/main/java/com/fishekai/game/Main.java
@@ -1,16 +1,22 @@
 package com.fishekai.game;
 
-import com.apps.util.SplashApp;
+import com.fishekai.engine.Fishekai;
 
-public class Main implements SplashApp{
+public class Main {
+
     public static void main(String[] args) {
+        Fishekai game = new Fishekai(); // create instance of the game
+//        game.welcome("images/fishekai_welcome.jpg"); // This only works when you build and run it like how we did with Jay's projects
 
-        Main app = new Main();
-        app.welcome("images/fishekai_welcome.jpg");
-        app.start();
+        // show title here
+        System.out.println("Replace me with the title.\n\n");
+
+        // insert prompt here
+        // if New Game, start the game
+        game.start(); // starts the game
+
+        // if Quit, ends the game
+
     }
-    public void start(){
-
-    }
-
 }
+
