d6ecf1cb6e8b71e6493641a0502e8118efbdf2cf
Luisito Canlas <Luisito.Canlas@tlgcohort.com>
2023-05-18 11:18:07 -0700

    Initial working stage, AB#82

--- src/main/java/com/fishekai/game/Main.java
+++ src/main/java/com/fishekai/game/Main.java
@@ -7,16 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Fishekai game = new Fishekai(); // create instance of the game
 //        game.welcome("images/fishekai_welcome.jpg"); // This only works when you build and run it like how we did with Jay's projects
-
-        // show title here
-        System.out.println("Replace me with the title.\n\n");
-
-        // insert prompt here
-        // if New Game, start the game
         game.start(); // starts the game
-
-        // if Quit, ends the game
-
     }
 }
 
