583f252e8ba99579c14edf04f70d89d1a8103b19
Chad Kellum <chad.kellum@tlgcohort.com>
2023-05-24 11:20:55 -0700

    Random quotes are said when talk command is used. AB#107

--- src/main/java/com/fishekai/objects/NPC.java
+++ src/main/java/com/fishekai/objects/NPC.java
@@ -37,20 +37,14 @@ public class NPC extends Character {
     }
 
     public void getRandomQuotes() {
-        for (String quote : randomQuotes
-        ) {
-            System.out.println(quote);
-
-        }
-
-    }
-
-    // methods
-    public String quotes() {
-        List<String> quotes = List.of("I require a shrubbery! Oh, wrong game. Sorry. I am the one who landed on this god-forsaken island. Did you know this island looks like a fish? I wonder what that means… Anyway, isn’t it weird that you can see me? Good luck with getting off this island.", "Hmmm… you’re talking to me again. What help could I possibly be? I’m dead!", "You know, you are a persistent thorn in my side. I’m just trying to spend the remainder of my non-existence in this lovely grove. Minding my own business!", "Still here? Go fishing already!");
         Random random = new Random();
-        return quotes.get(random.nextInt(quotes.size()));
+        int randomIndex = random.nextInt(randomQuotes.length);
+        String randomQuote = randomQuotes[randomIndex];
+        System.out.printf("The %s says: '%s'\n\n", getType(),randomQuote);
     }
+
+
+
 
     // for internal testing
 //    public static void main(String[] args) {
